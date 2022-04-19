18fc6d94fdf656846b955b268b6d191a9da7d6bb
Brady Moon <bradygmoon@gmail.com>
2022-04-19 12:34:08 -0400

    Update risk with threshold

--- RiskCost.py
+++ RiskCost.py
@@ -9,18 +9,24 @@ class RiskCost:
         self.cost = np.zeros(self.slopes.shape)
         self.inflated_cost = np.zeros(self.slopes.shape)
 
-        window = 2
+        window = 2 # window size for calculating inflated cost
+        thresh = 45 # threshold for inflating cost
+        inflate_scalar = 10 # scalar for inflating cost
         for i in range(self.slopes.shape[0]):
             for j in range(self.slopes.shape[1]):
                 # set cost as slope value
                 self.cost[i,j] = self.slopes[i,j] 
 
-                # set inflated cost as the maximum of the surrounding costs
+                
+                # set inflated cost as the maximum of the surrounding costs if above threshold
                 i_bottom = max(0, min(i-window, self.slopes.shape[0]))
                 i_top = max(0, min(i+window, self.slopes.shape[0]))
                 j_left = max(0, min(j-window, self.slopes.shape[1]))
                 j_right = max(0, min(j+window, self.slopes.shape[1]))
-                self.inflated_cost[i,j] = np.amax(self.slopes[i_bottom:i_top, j_left:j_right])
+                inflate_cost = 0
+                if np.amax(self.slopes[i_bottom:i_top, j_left:j_right]) >= thresh:
+                    inflate_cost = np.amax(self.slopes[i_bottom:i_top, j_left:j_right])*inflate_scalar
+                self.inflated_cost[i,j] = inflate_cost
         
                 
     def get_cost(self, x, y):
